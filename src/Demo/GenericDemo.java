d98d8ea57ecc3fa0bde732f7d2e1ce298a062e35
qqiang00 <qiang_ye@163.com>
2016-12-21 12:24:59 -0500

    add generic demos

---
+++ src/Demo/GenericDemo.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package Demo;
+
+/**
+ * @author qiang
+ * 演示了泛型、在类内部定义内部类及使用内部类的方法
+ *
+ */
+public class GenericDemo {
+	public class Box<T>{
+		private T t;
+		
+		public void set(T t){
+			this.t= t;
+		}
+		
+		public T get(){
+			return t;
+		}
+		
+		public <U extends Number> void inspect(U u){
+			System.out.println("T: " + t.getClass().getName());
+			System.out.println("U: " + u.getClass().getName());
+		}
+		
+	}
+	
+	public class NaturalNumber<T extends Integer>{
+		private T n;
+		public NaturalNumber(T n){ this.n = n;}
+		public boolean isEven(){
+			return n.intValue() % 2 == 0;
+		}
+		public T get(){
+			return (T)n;
+		}
+	}
+	
+	public static void main(String[] args){
+		GenericDemo demo = new GenericDemo();
+		Box<Integer> integerBox = demo.new Box<Integer>();
+		integerBox.set(new Integer(10));
+		//integerBox.inspect("some text"); //error : this is still String
+		integerBox.inspect(3.1315926d);	
+		
+		NaturalNumber<Integer> num = demo.new NaturalNumber<Integer>(34);
+		System.out.printf("is %s even? %s ",num.get(), num.isEven());
+		
+	}
+}
--- src/Demo/StringDemo.java
+++ src/Demo/StringDemo.java
@@ -1,42 +1,68 @@
 package Demo;
 
 public class StringDemo {
-	public static void main(String[] args){
+	
+	public static void stringDemo(){
 		//String greeting = "Hello World!";
 		char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
 		String helloString = new String(helloArray);
 		System.out.println(helloString);
-		
+
 		String palindrome = "Dot saw I was Tod";
 		int len = palindrome.length();
 		char[] tempCharArray = new char[len];
 		char[] charArray = new char[len];
-		
+
 		//put original string in an array of chars
 		for (int i = 0; i < len; i++){
 			tempCharArray[i] = palindrome.charAt(i);
 		}
 		System.out.println(tempCharArray);
-		
+
 		//reverse array of chars
 		for (int j = 0; j < len; j++){
 			charArray[j] = tempCharArray[len-1-j];
 		}
 		String reversPalindrome = new String(charArray);
 		System.out.println(reversPalindrome);
-		
+
 		//create format strings
 		String fs;
 		float floatVar = 3.14f;
 		int intVar = 314;
 		String stringVar = "Hello World.";
 		fs = String.format("The value of the float " +
-		                   "variable is %f, \nwhile " +
-		                   "the value of the " + 
-		                   "integer variable is %d, \n" +
-		                   "and the string is %s",
-		                   floatVar, intVar, stringVar);
+				"variable is %f, \nwhile " +
+				"the value of the " + 
+				"integer variable is %d, \n" +
+				"and the string is %s",
+				floatVar, intVar, stringVar);
 		System.out.println(fs);
+	}
+	public static <T> void appendPrint(StringBuilder sb, T b){
+		sb.append(" ");
+		sb.append((T)b);
+		System.out.println(sb);
+	}
+	public static void stringBuilderDemo(){
+		String palindrome = "Dot saw I was Tod";
+		StringBuilder sb = new StringBuilder(palindrome);
+		sb.reverse();
+		System.out.println(sb);
+		appendPrint(sb,true);
+		appendPrint(sb,'c');
+		char[] arr = {'a', 'r', 'r', 'a', 'y'};
+		appendPrint(sb, arr);
+		appendPrint(sb, 3.4124D);
+		appendPrint(sb, 23);
+		appendPrint(sb, 3.13f);
+		appendPrint(sb, 123412l);
+		appendPrint(sb, "string");
+		sb.reverse();
+		appendPrint(sb, " ");
+	}
+	public static void main(String[] args){
+		stringBuilderDemo();
 	}
 
 }
